d0af9614bf3a9c58f20e1c2b550da3995ce202a3
Luu Yen Nhi <luuenhi03@gmail.com>
2023-04-23 18:08:50 +0700

    java basic

--- Calculator/src/TinhToan/TinhChanLe.java
+++
@@ -1,5 +0,0 @@
-package TinhToan;
-
-public class TinhChanLe {
-
-}
--- Calculator/src/TinhToan/TinhTong.java
+++
@@ -1,7 +0,0 @@
-package TinhToan;
-
-public class TinhTong {
-	 public static void main(String[]args) {
-		 System.out.println("Tinh tong");
-	 }
-}
